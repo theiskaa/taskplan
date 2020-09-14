89e11c598541058076875d7e778c3a2b5e4604d8
theiskaa <59066341+theiskaa@users.noreply.github.com>
2020-09-14 16:47:37 +0400

    animations

--- lib/UI/widgets/animation_delete_button.dart
+++ lib/UI/widgets/animation_delete_button.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 class AnimationDeleteButton extends StatefulWidget {
@@ -7,7 +5,8 @@ class AnimationDeleteButton extends StatefulWidget {
   _AnimationDeleteButtonState createState() => _AnimationDeleteButtonState();
 }
 
-class _AnimationDeleteButtonState extends State<AnimationDeleteButton> {
+class _AnimationDeleteButtonState extends State<AnimationDeleteButton>
+    with TickerProviderStateMixin {
   AnimationController _animationController;
   Animation<Color> _colorAnimation;
   Animation<double> _sizeAnimation;
@@ -16,12 +15,49 @@ class _AnimationDeleteButtonState extends State<AnimationDeleteButton> {
   @override
   void initState() {
     super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(
+        milliseconds: 300,
+      ),
+    );
+
+    _colorAnimation = TweenSequence(
+      <TweenSequenceItem<Color>>[
+        TweenSequenceItem(
+            tween: Tween(begin: Colors.white, end: Colors.red), weight: 38),
+        TweenSequenceItem(
+            tween: Tween(begin: Colors.red, end: Colors.white), weight: 38),
+      ],
+    ).animate(_animationController) as Animation<Color>;
+
+    _sizeAnimation = TweenSequence(
+      <TweenSequenceItem<double>>[
+        TweenSequenceItem(tween: Tween<double>(begin: 35, end: 38), weight: 38),
+        TweenSequenceItem(tween: Tween<double>(begin: 38, end: 35), weight: 38),
+      ],
+    ).animate(_animationController);
   }
+
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-      animation: _animationController, 
-      builder: null,
+      animation: _animationController,
+      builder: (context, index) {
+        return IconButton(
+          icon: Icon(
+            Icons.delete,
+            color: _colorAnimation.value,
+            size: _sizeAnimation.value,
+          ),
+          onPressed: () {
+            isDelete
+                ? _animationController.reverse()
+                : _animationController.forward();
+          },
+        );
+      },
     );
   }
 }

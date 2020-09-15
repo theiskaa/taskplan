4f75a554b1960c96b621414405813a7ea46e9159
theiskaa <59066341+theiskaa@users.noreply.github.com>
2020-09-15 15:02:04 +0400

    fab with opacity

--- lib/UI/widgets/fab_buttons.dart
+++ lib/UI/widgets/fab_buttons.dart
@@ -11,17 +11,27 @@ class FAB extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPress,
-      child: Container(
-        height: 70,
-        width: 70,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(100),
-        ),
-        child: Center(
-          child: Image.asset('assets/plus.png', height: 35),
+    return TweenAnimationBuilder(
+      tween: Tween<double>(begin: 0, end: 1),
+      duration: Duration(seconds: 1),
+      builder: (BuildContext context, double value, Widget child) {
+        return Opacity(
+          opacity: value,
+          child: child,
+        );
+      },
+      child: GestureDetector(
+        onTap: onPress,
+        child: Container(
+          height: 70,
+          width: 70,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: Center(
+            child: Image.asset('assets/plus.png', height: 35),
+          ),
         ),
       ),
     );
